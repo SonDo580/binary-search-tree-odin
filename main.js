fd42d8642ca2d98353b360caf484781a2219cc4f
Son Do <dominhson580@gmail.com>
2022-09-08 15:48:06 +0700

    Add prettyPrint() to console.log the tree

--- main.js
+++ main.js
@@ -24,6 +24,16 @@ function TreeFactory(array) {
 
     return root;
   }
+
+  const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
 }
 
 // SORTING ALGORITHMS
