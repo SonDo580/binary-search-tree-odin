1f3af5ee8a3378a7240406a28d92b25422fb8b08
Son Do <dominhson580@gmail.com>
2022-09-08 15:24:48 +0700

    Creating a new version of quickSort

--- main.js
+++ main.js
@@ -19,53 +19,63 @@ function TreeFactory(array) {
 }
 
 // SORTING ALGORITHMS
-function _swap(array, i, j) {
-  let temp = array[i];
-  array[i] = array[j];
-  array[j] = temp;
-}
+// function _swap(array, i, j) {
+//   let temp = array[i];
+//   array[i] = array[j];
+//   array[j] = temp;
+// }
 
-function _partition(array, left, right) {
-  let pivot = array[Math.floor((left + right) / 2)]; // use the middle element as pivot
+// function _partition(array, left, right) {
+//   let pivot = array[Math.floor((left + right) / 2)]; // use the middle element as pivot
 
-  let i = left; // left pointer
-  let j = right; // right pointer
+//   let i = left; // left pointer
+//   let j = right; // right pointer
 
-  while (i <= j) {
-    while (array[i] < pivot) {
-      i++;
-    }
-    while (array[j] > pivot) {
-      j--;
-    }
-    if (i <= j) {
-      _swap(array, i, j);
-      i++;
-      j--;
-    }
+//   while (i <= j) {
+//     while (array[i] < pivot) {
+//       i++;
+//     }
+//     while (array[j] > pivot) {
+//       j--;
+//     }
+//     if (i <= j) {
+//       _swap(array, i, j);
+//       i++;
+//       j--;
+//     }
+//   }
+
+//   return i; // pivot is now sorted and remain at this same index
+// }
+
+// function quickSort(array, left, right) {
+//   let index;
+
+//   if (array.length > 1) {
+//     index = _partition(array, left, right); // (new) index of the pivot
+
+//     // sort the elements on the left side of the pivot
+//     if (left < index - 1) {
+//       quickSort(array, left, index - 1);
+//     }
+
+//     // sort the elements on the right side of the pivot
+//     if (index < right) {
+//       quickSort(array, index, right);
+//     }
+//   }
+
+//   return array;
+// }
+
+function quickSort(array) {
+  const lessList = [];
+  const moreList = [];
+  const pivotList = [];
+
+  if (array.length <= 1) {
+    return array;
   }
-
-  return i; // pivot is now sorted and remain at this same index
-}
-
-function quickSort(array, left, right) {
-  let index;
-
-  if (array.length > 1) {
-    index = _partition(array, left, right); // (new) index of the pivot
-
-    // sort the elements on the left side of the pivot
-    if (left < index - 1) {
-      quickSort(array, left, index - 1);
-    }
-
-    // sort the elements on the right side of the pivot
-    if (index < right) {
-      quickSort(array, index, right);
-    }
-  }
-
-  return array;
 }
 
 // REMOVE DUPLICATES
